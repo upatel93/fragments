bce0307fc0a96736c94a9581581606e29ecb0e37
upatel93 <upatel69@myseneca.ca>
2023-05-27 21:50:47 -0400

    Breaking code for unused variable

--- src/index.js
+++ src/index.js
@@ -5,6 +5,8 @@
 // NOTE: we only need to do this once in our app's main entry point.
 require('dotenv').config();
 
+const unneededVariable = 'This variable is never used';
+
 // We want to log any crash cases so we can debug later from logs.
 const logger = require('./logger');
 
