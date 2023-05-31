d0e5de205ef4ec3ca2fbec274795ef2d043ee2e2
upatel93 <upatel69@myseneca.ca>
2023-05-31 14:19:37 -0400

    Add Hash module and tested with jest and Fragments UI

--- src/routes/api/post.js
+++ src/routes/api/post.js
@@ -3,14 +3,17 @@
 // Fragment Class
 const { Fragment } = require('../../model/fragment');
 
+// Hashing MOdule import
+const plseaseHashIt = require('../../hash');
+
 module.exports = async (req, res) => {
-  let data = new Fragment({ ownerId: 'TestOwner', type: 'text/plain' });
+  let data = new Fragment({ ownerId: `${plseaseHashIt(req.user)}`, type: req.headers["content-type"] });
   await data.save();
   await data.setData(req.body);
   let sendData = {
     status: 'ok',
     fragment: data,
   };
-  res.set('Location',`${process.env.API_URL}/v1/fragment/${data.id}`)
+  res.set('Location',`${process.env.API_URL || req.headers.host}/v1/fragment/${data.id}`)
   res.status(201).json(sendData);
 };
