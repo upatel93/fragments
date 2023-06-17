3bbe37ef30290192c4cfd090219d8ef3f56ef4a6
upatel93 <upatel69@myseneca.ca>
2023-06-17 10:54:51 -0400

    Added Utility functions to support and modulurize code in get and post extension conversion routes

--- src/model/data/utils.js
+++ src/model/data/utils.js
@@ -1,3 +1,8 @@
+/* global Buffer */
+const sharp = require('sharp');
+const markdownIt = require('markdown-it')();
+const contentType = require('content-type');
+
 /**
  * Checks if a conversion is possible from the original type to the requested type.
  * @param {string} originalType - The original type of the fragment.
@@ -29,8 +34,7 @@ module.exports.isConversionPossible = (originalType, requestedType) => {
   }
 
   return false; // Conversion is not possible
-}
-
+};
 
 /**
  * Separates an identifier string with an extension into its constituent parts,
@@ -57,6 +61,7 @@ module.exports.separateIdExtensionAndMediaType = (idWithExtension) => {
     json: 'application/json',
     png: 'image/png',
     jpg: 'image/jpeg',
+    jpeg: 'image/jpeg',
     webp: 'image/webp',
     gif: 'image/gif',
   };
@@ -84,7 +89,7 @@ module.exports.separateIdExtensionAndMediaType = (idWithExtension) => {
       mediaType: '',
     };
   }
-}
+};
 
 /**
  * Checks if an identifier string has an extension.
@@ -105,13 +110,108 @@ module.exports.hasExtension = (id) => {
 
   var lastDotIndex = id.lastIndexOf('.');
   return lastDotIndex !== -1 && lastDotIndex < id.length - 1;
+};
+
+/**
+ * Set's the fragment's data in the database
+ * @param {Buffer} data
+ * @returns string
+ */
+module.exports.rawJSONtoTextPlain = (rawData) => {
+  return rawData.toString('utf8');
+};
+
+/**
+ * Converts a fragment from Markdown to HTML.
+ * @param {Buffer|string} rawData - The raw binary data of the fragment.
+ * @returns {string} - The converted HTML.
+ */
+function convertMarkdownToHTML(rawData) {
+  const markdownData = rawData instanceof Buffer ? rawData.toString() : rawData;
+  return markdownIt.render(markdownData);
 }
 
 /**
-   * Set's the fragment's data in the database
-   * @param {Buffer} data
-   * @returns string
-   */
-module.exports.rawJSONtoTextPlain = (rawData) => {
- return rawData.toString('utf8');
+ * Converts a fragment from HTML or Markdown to plain text.
+ * @param {Buffer|string} rawData - The raw binary data of the fragment.
+ * @param {string} fromType - The type of the fragment to convert from.
+ * @returns {string} - The converted plain text.
+ */
+function convertToPlainText(rawData, fromType) {
+  let data = rawData instanceof Buffer ? rawData.toString() : rawData;
+  if (fromType === 'text/html') {
+    return data.replace(/<[^>]+>/g, '');
+  } else if (fromType === 'text/markdown') {
+    return markdownIt.render(data).replace(/<[^>]+>/g, '');
+  }
 }
+
+/**
+ * Converts a fragment from JSON to plain text.
+ * @param {Buffer|string} rawData - The raw binary data of the fragment.
+ * @returns {string} - The converted plain text.
+ * @throws {Error} - Throws an error if the conversion fails.
+ */
+function convertJSONToText(rawData) {
+  try {
+    const jsonData = JSON.parse(rawData);
+    return JSON.stringify(jsonData, null, 2);
+  } catch (error) {
+    throw new Error('Failed to convert JSON to plain text');
+  }
+}
+
+/**
+ * Converts a fragment from one image format to another.
+ * @param {Buffer} rawData - The raw binary data of the fragment.
+ * @param {string} toExt - The extension representing the desired conversion type.
+ * @returns {Buffer} - The converted image buffer.
+ */
+async function convertImageFormat(rawData, toExt) {
+  const image = sharp(rawData);
+  image.toFormat(toExt);
+  return await image.toBuffer();
+}
+
+/**
+ * Converts a fragment from one type to another based on the provided extension.
+ * @param {Buffer|string} rawBinaryData - The raw binary data of the fragment.
+ * @param {string} fromType - The type of the fragment to convert from.
+ * @param {string} toExt - The extension representing the desired conversion type.
+ * @returns {Buffer|string|null} - The converted data, or null if the conversion fails.
+ * @throws {Error} - Throws an error if the conversion is not supported or encounters an error.
+ */
+module.exports.convertFragment = async (rawBinaryData, fromType, toExt, toType) => {
+  if (fromType === 'text/markdown' && toExt === 'html') {
+    return convertMarkdownToHTML(rawBinaryData);
+  } else if ((fromType === 'text/html' || fromType === 'text/markdown') && toExt === 'txt') {
+    return convertToPlainText(rawBinaryData, fromType);
+  } else if (fromType === 'application/json' && toExt === 'txt') {
+    return convertJSONToText(rawBinaryData);
+  } else if (
+    fromType.startsWith('image/') &&
+    ['png', 'jpg', 'jpeg', 'webp', 'gif'].includes(toExt)
+  ) {
+    return await convertImageFormat(rawBinaryData, toExt);
+  } else if (fromType === toType) {
+    return rawBinaryData;
+  } else {
+    throw new Error('Unsupported conversion');
+  }
+};
+
+module.exports.validateContentType = async (reqBody, contentTypeHeader) => {
+  const { type } = contentType.parse(contentTypeHeader);
+
+  if (type === 'application/json') {
+    JSON.parse(reqBody);
+    return;
+  } else if (type.includes('image')) {
+    await sharp(reqBody).metadata();
+    return;
+  } else if (type === 'text/html' || type === 'text/plain' || type === 'text/markdown') {
+    return;
+  } else {
+    throw new Error('Unsupported content type');
+  }
+};
